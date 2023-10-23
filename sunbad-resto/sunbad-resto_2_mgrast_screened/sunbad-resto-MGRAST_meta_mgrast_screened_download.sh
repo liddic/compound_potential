3f8379adfeb205d5913fe0be108c4067a3ecdd59
Craig Liddicoat <81080897+liddic@users.noreply.github.com>
2023-10-23 17:15:58 +1030

    Add files via upload

---
+++ sunbad-resto/sunbad-resto_2_mgrast_screened/sunbad-resto-MGRAST_meta_mgrast_screened_download.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --time=1-0
+#SBATCH --mem=64000M
+#SBATCH --cpus-per-task=16
+
+/home/lidd0026/miniconda3/bin/mg-download.py --metagenome mgm4679658.3 --file 299.1
+/home/lidd0026/miniconda3/bin/mg-download.py --metagenome mgm4679659.3 --file 299.1
+/home/lidd0026/miniconda3/bin/mg-download.py --metagenome mgm4679660.3 --file 299.1
+/home/lidd0026/miniconda3/bin/mg-download.py --metagenome mgm4679661.3 --file 299.1
+/home/lidd0026/miniconda3/bin/mg-download.py --metagenome mgm4679662.3 --file 299.1
+/home/lidd0026/miniconda3/bin/mg-download.py --metagenome mgm4679663.3 --file 299.1
+/home/lidd0026/miniconda3/bin/mg-download.py --metagenome mgm4679664.3 --file 299.1
+/home/lidd0026/miniconda3/bin/mg-download.py --metagenome mgm4679665.3 --file 299.1
+/home/lidd0026/miniconda3/bin/mg-download.py --metagenome mgm4679666.3 --file 299.1
+/home/lidd0026/miniconda3/bin/mg-download.py --metagenome mgm4679667.3 --file 299.1
+/home/lidd0026/miniconda3/bin/mg-download.py --metagenome mgm4679668.3 --file 299.1
+/home/lidd0026/miniconda3/bin/mg-download.py --metagenome mgm4679669.3 --file 299.1
+/home/lidd0026/miniconda3/bin/mg-download.py --metagenome mgm4679670.3 --file 299.1
+/home/lidd0026/miniconda3/bin/mg-download.py --metagenome mgm4679671.3 --file 299.1
+/home/lidd0026/miniconda3/bin/mg-download.py --metagenome mgm4679672.3 --file 299.1
