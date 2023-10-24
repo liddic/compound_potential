c81dc4263e9475eb82d772ced1353bec08096fe8
Craig Liddicoat <81080897+liddic@users.noreply.github.com>
2023-10-24 12:38:27 +1030

    Add files via upload

---
+++ jie-acvd/jacvd_3_superfocus_fxns/job_files/submission_superfocus_ERR2017574.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH --ntasks=1
+#SBATCH --time=4-0
+#SBATCH --mem=64000M
+#SBATCH --cpus-per-task=16
+#SBATCH --nodes=1
+export TMPDIR=/scratch/user/lidd0026/temp/ERR2017574
+superfocus -q /scratch/user/lidd0026/jie-acvd/jacvd_2_fastp_qc/ERR2017574_R1.good.fastq -dir /scratch/user/lidd0026/jie-acvd/jacvd_3_superfocus_fxns/superfocus_out_ERR2017574 -a diamond -db DB_100 --alternate_directory /home/lidd0026/miniconda3/lib/python3.8/site-packages/superfocus_app
