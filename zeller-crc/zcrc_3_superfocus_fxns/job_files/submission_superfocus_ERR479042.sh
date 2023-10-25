670c5d8036726b043dd56fd2ed23f68d98a8c75c
Craig Liddicoat <81080897+liddic@users.noreply.github.com>
2023-10-25 13:14:29 +1030

    Add files via upload

---
+++ zeller-crc/zcrc_3_superfocus_fxns/job_files/submission_superfocus_ERR479042.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH --ntasks=1
+#SBATCH --time=4-0
+#SBATCH --mem=64000M
+#SBATCH --cpus-per-task=16
+#SBATCH --nodes=1
+export TMPDIR=/scratch/user/lidd0026/temp/ERR479042
+superfocus -q /scratch/user/lidd0026/zeller-crc/zcrc_2_fastp_qc/ERR479042_R1.good.fastq -dir /scratch/user/lidd0026/zeller-crc/zcrc_3_superfocus_fxns/superfocus_out_ERR479042 -a diamond -db DB_100 --alternate_directory /home/lidd0026/miniconda3/lib/python3.8/site-packages/superfocus_app
