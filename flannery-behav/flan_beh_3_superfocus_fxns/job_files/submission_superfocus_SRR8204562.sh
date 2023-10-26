abad5250cd8c7a43b40d745049e0bc93e8c3ec23
Craig Liddicoat <81080897+liddic@users.noreply.github.com>
2023-10-26 11:30:21 +1030

    Add files via upload

---
+++ flannery-behav/flan_beh_3_superfocus_fxns/job_files/submission_superfocus_SRR8204562.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH --ntasks=1
+#SBATCH --time=4-0
+#SBATCH --mem=64000M
+#SBATCH --cpus-per-task=16
+#SBATCH --nodes=1
+export TMPDIR=/scratch/user/lidd0026/temp/SRR8204562
+superfocus -q /scratch/user/lidd0026/flannery-behav/flan_beh_2_fastp_qc/SRR8204562_R1.good.fastq -dir /scratch/user/lidd0026/flannery-behav/flan_beh_3_superfocus_fxns/superfocus_out_SRR8204562 -a diamond -db DB_100 --alternate_directory /home/lidd0026/miniconda3/lib/python3.8/site-packages/superfocus_app
