9ebea40787826f4de46b89d7f22ef5178064bf6c
Craig Liddicoat <81080897+liddic@users.noreply.github.com>
2023-10-26 11:05:27 +1030

    Add files via upload

---
+++ flannery-behav/flan_beh_1_meta_raw/flannery-behav_1_meta_raw_download.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --time=4-0
+#SBATCH --mem=64000M
+#SBATCH --cpus-per-task=16
+
+fastq-dump --outdir /scratch/user/lidd0026/flannery-behav/flan_beh_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR8204551
+fastq-dump --outdir /scratch/user/lidd0026/flannery-behav/flan_beh_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR8204552
+fastq-dump --outdir /scratch/user/lidd0026/flannery-behav/flan_beh_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR8204553
+fastq-dump --outdir /scratch/user/lidd0026/flannery-behav/flan_beh_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR8204554
+fastq-dump --outdir /scratch/user/lidd0026/flannery-behav/flan_beh_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR8204555
+fastq-dump --outdir /scratch/user/lidd0026/flannery-behav/flan_beh_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR8204556
+fastq-dump --outdir /scratch/user/lidd0026/flannery-behav/flan_beh_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR8204557
+fastq-dump --outdir /scratch/user/lidd0026/flannery-behav/flan_beh_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR8204558
+fastq-dump --outdir /scratch/user/lidd0026/flannery-behav/flan_beh_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR8204559
+fastq-dump --outdir /scratch/user/lidd0026/flannery-behav/flan_beh_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR8204560
+fastq-dump --outdir /scratch/user/lidd0026/flannery-behav/flan_beh_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR8204561
+fastq-dump --outdir /scratch/user/lidd0026/flannery-behav/flan_beh_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR8204562
+fastq-dump --outdir /scratch/user/lidd0026/flannery-behav/flan_beh_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR8204563
+fastq-dump --outdir /scratch/user/lidd0026/flannery-behav/flan_beh_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR8204564
+fastq-dump --outdir /scratch/user/lidd0026/flannery-behav/flan_beh_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR8204565
+fastq-dump --outdir /scratch/user/lidd0026/flannery-behav/flan_beh_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR8204566
+fastq-dump --outdir /scratch/user/lidd0026/flannery-behav/flan_beh_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR8204567
+fastq-dump --outdir /scratch/user/lidd0026/flannery-behav/flan_beh_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR8204568
+fastq-dump --outdir /scratch/user/lidd0026/flannery-behav/flan_beh_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR8204569
+fastq-dump --outdir /scratch/user/lidd0026/flannery-behav/flan_beh_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR8204570
+fastq-dump --outdir /scratch/user/lidd0026/flannery-behav/flan_beh_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR8204571
+fastq-dump --outdir /scratch/user/lidd0026/flannery-behav/flan_beh_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR8204572
+fastq-dump --outdir /scratch/user/lidd0026/flannery-behav/flan_beh_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR8204573
+fastq-dump --outdir /scratch/user/lidd0026/flannery-behav/flan_beh_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR8204574
+fastq-dump --outdir /scratch/user/lidd0026/flannery-behav/flan_beh_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR8204575
+fastq-dump --outdir /scratch/user/lidd0026/flannery-behav/flan_beh_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR8204576
+fastq-dump --outdir /scratch/user/lidd0026/flannery-behav/flan_beh_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR8204577
+fastq-dump --outdir /scratch/user/lidd0026/flannery-behav/flan_beh_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR8204578
+fastq-dump --outdir /scratch/user/lidd0026/flannery-behav/flan_beh_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR8204579
+fastq-dump --outdir /scratch/user/lidd0026/flannery-behav/flan_beh_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR8204580
+fastq-dump --outdir /scratch/user/lidd0026/flannery-behav/flan_beh_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR8204581
+fastq-dump --outdir /scratch/user/lidd0026/flannery-behav/flan_beh_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR8204582
+fastq-dump --outdir /scratch/user/lidd0026/flannery-behav/flan_beh_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR8204583
+fastq-dump --outdir /scratch/user/lidd0026/flannery-behav/flan_beh_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR8204584
+fastq-dump --outdir /scratch/user/lidd0026/flannery-behav/flan_beh_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR8204585
+fastq-dump --outdir /scratch/user/lidd0026/flannery-behav/flan_beh_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR8204586
+fastq-dump --outdir /scratch/user/lidd0026/flannery-behav/flan_beh_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR8204587
+fastq-dump --outdir /scratch/user/lidd0026/flannery-behav/flan_beh_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR8204588
+fastq-dump --outdir /scratch/user/lidd0026/flannery-behav/flan_beh_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR8204589
+fastq-dump --outdir /scratch/user/lidd0026/flannery-behav/flan_beh_1_meta_raw --skip-technical --readids --read-filter pass --dumpbase --clip --split-3 SRR8204590
