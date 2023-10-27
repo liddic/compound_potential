01c032a8e9ecd47b11ede0bd728b5dd168dd91e4
Craig Liddicoat <81080897+liddic@users.noreply.github.com>
2023-10-27 10:43:40 +1030

    Update submission_superfocus_10931.sh

--- pcan-resto/nz_3_superfocus_fxns/job_files/submission_superfocus_10931.sh
+++ pcan-resto/nz_3_superfocus_fxns/job_files/submission_superfocus_10931.sh
@@ -5,4 +5,4 @@
 #SBATCH --cpus-per-task=32
 #SBATCH --nodes=1
 export TMPDIR=/scratch/user/lidd0026/temp/10931
-superfocus -q /scratch/user/lidd0026/nz/pilot/nz_2_fastp_qc/10931_R1.good.fastq -dir /scratch/user/lidd0026/nz/pilot/nz_5_superfocus_fxns/superfocus_out_10931 -a diamond -db DB_100 --alternate_directory /home/lidd0026/miniconda3/lib/python3.8/site-packages/superfocus_app
+superfocus -q /scratch/user/lidd0026/nz/pilot/nz_2_fastp_qc/10931_R1.good.fastq -dir /scratch/user/lidd0026/nz/pilot/nz_3_superfocus_fxns/superfocus_out_10931 -a diamond -db DB_100 --alternate_directory /home/lidd0026/miniconda3/lib/python3.8/site-packages/superfocus_app
